17be0466cb30f89ac437b2a8fa8c2582a2fc613b
ice-blaze <franketip@gmail.com>
2015-03-19 18:46:08 +0100

    add the reading file part

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <cstdio>
 #include <ctime>
 #include <cstdlib>
@@ -6,52 +7,19 @@
 
 
 using namespace std;
-//const int K = 3;
-//typedef struct NOEUD
-//{
-//    double cle[K];
-//    struct NOEUD *g;
-//    struct NOEUD *d;
-//} NOEUD;
-//
-//void inserer(NOEUD **arbre, NOEUD *element, int prof)
-//{
-//    if (*arbre == NULL)
-//        (*arbre) = element;
-//    else if ((*arbre)->cle[prof%K] < element->cle[prof%K])
-//        inserer(&(*arbre)->d, element, prof+1);
-//    else
-//        inserer(&(*arbre)->g, element, prof+1);
-//}
-//
-//int rapporter(double cle[K], double min[K], double max[K])
-//{
-//    int i, dedans = 1;
-//    for (i = 0; i < K && dedans; i++)
-//        dedans = dedans && min[i] <= cle[i] && max[i] >= cle[i];
-//    if (dedans) // Rapporter le point
-//    {
-//        for (i = 0; i < K; i++)
-//            printf("%f ", cle[i]);
-//        printf("\n");
-//    }
-//    return dedans;
-//}
-//void recherche(NOEUD *arbre, double min[K], double max[K],int prof)
-//{
-//    if (arbre) // arbre non vide
-//    {
-//        if (arbre->cle[prof%K] >= min[prof%K])
-//            recherche(arbre->g, min, max, prof+1);
-//        rapporter(arbre->cle, min, max);
-//        if (arbre->cle[prof%K] <= max[prof%K])
-//            recherche(arbre->d, min, max, prof+1);
-//    }
-//}
 
-// perf 5.4
 int main()
 {
+    ifstream fin("data/scan6_zoneB.bin", ios::binary);
+    double stationx,stationy,stationz;
+    unsigned int nb_points, donee_supp;
+
+    fin.read(reinterpret_cast<char*>(&stationx), sizeof stationx);
+    fin.read(reinterpret_cast<char*>(&stationy), sizeof stationy);
+    fin.read(reinterpret_cast<char*>(&stationz), sizeof stationz);
+
+    fin.read(reinterpret_cast<char*>(&nb_points), sizeof nb_points);
+    fin.read(reinterpret_cast<char*>(&donee_supp), sizeof donee_supp);
     clock_t t1=clock();
     clock_t t2=clock();
 
@@ -59,20 +27,18 @@ int main()
     Node* racine = new Node();
     racine->key[0] = racine->key[1] = racine->key[2] = 0;
 
-    const long NB_MAX = 60000000;
+//    const long NB_MAX = 60000000;
 
-    Node* noeuds= new Node[60000000];
+    Node* noeuds= new Node[nb_points];
 
-    for(long i=0; i<NB_MAX; i++)
+    for(long i=0; i<nb_points; i++)
     {
-// NOEUD* noeud = new NOEUD();
-// noeud->cle[0] = rand();
-// noeud->cle[1] = rand();
-// noeud->cle[2] = rand();
-// inserer(&racine,noeud,0);
-        noeuds[i].key[0] = rand();
-        noeuds[i].key[1] = rand();
-        noeuds[i].key[2] = rand();
+        fin.read(reinterpret_cast<char*>(&noeuds[i].key[0]), sizeof noeuds[i].key[0]);
+        fin.read(reinterpret_cast<char*>(&noeuds[i].key[1]), sizeof noeuds[i].key[1]);
+        fin.read(reinterpret_cast<char*>(&noeuds[i].key[2]), sizeof noeuds[i].key[2]);
+
+        Node::add(&racine,&noeuds[i]);
+
         if(i%10000000==0)
             cout << "complete : " << i << endl;
     }
