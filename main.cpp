b3054ff7526c6caec9be3e2cc6c952f77515a1e1
ice-blaze <franketip@gmail.com>
2015-03-19 17:20:39 +0100

    Add a reading code for the inputs files

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    std::ifstream fin("data/scan6_zoneB.bin", ios::binary);
+    double stationx,stationy,stationz;
+    unsigned int nb_points, donee_supp;
+
+    fin.read(reinterpret_cast<char*>(&stationx), sizeof stationx);
+    fin.read(reinterpret_cast<char*>(&stationy), sizeof stationy);
+    fin.read(reinterpret_cast<char*>(&stationz), sizeof stationz);
+
+    fin.read(reinterpret_cast<char*>(&nb_points), sizeof nb_points);
+
+//    fin >> stationx;
+//    fin >> stationy;
+//    fin >> stationz;
+
+//    fin >> nb_points;
+//    fin >> donee_supp;
+//    std::cout << d << std::endl;
+    cout << "station : " << stationx << " " << stationy << " " << stationz << endl;
+    cout << "nb points " << nb_points << endl;
     return 0;
 }
