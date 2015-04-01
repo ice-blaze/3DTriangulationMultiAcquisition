abfceb77f7bb2765f5b9fd19642b274c0e0467b9
ice-blaze <franketip@gmail.com>
2015-04-01 20:45:01 +0200

    Add the convert file from cout to ply file

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 #include <cassert>
 #include <list>
 #include "Chrono.h"
+#include <string>
+#include <cstdint>
 
 const int MUL = 1000000;
 int convertFloatInt(float _val) { return _val*MUL; }
@@ -23,7 +25,8 @@ int main()
 // construction from a list of points :
     list<Point> L;
 
-    ifstream fin("data/scan6_zoneB.bin", ios::binary);
+//    ifstream fin("data/scan6_zoneB.bin", ios::binary);
+    ifstream fin("data/s6s100000.bin", ios::binary);
     double stationx,stationy,stationz;
     unsigned int nb_points, donee_supp;
 
@@ -46,69 +49,83 @@ int main()
 
         // TODO à test
 //        L.insert(Point(convertFloatInt(x),convertFloatInt(y),convertFloatInt(z)));
-        L.push_front(Point(convertFloatInt(x),convertFloatInt(y),convertFloatInt(z)));
+        L.push_front(Point(x,y,z));
 
         if(i%1000000==0){
             cout << "complete : " << i << endl;
         }
     }
 
+    L.clear();
+    L.push_front(Point(0,0,0));
+    L.push_front(Point(1,0,0));
+    L.push_front(Point(0,1,0));
+    L.push_front(Point(0,0,1));
+    L.push_front(Point(1,1,0));
+    L.push_front(Point(1,0,1));
+    L.push_front(Point(0,1,1));
+    L.push_front(Point(1,1,1));
+
     Delaunay T(L.begin(), L.end());
     chrono.printTime();
+    chrono.restart();
 
-//    T.
-//    ofstream oFileT("output",ios::out);
-//    oFileT << T;
-//    Triangulation T1;
-//    ifstream iFileT("output",ios::in);
-//    return 0;
-//}
-//    for (typename std::vector<Point>::const_iterator p = points.begin(), end = points.end();
-//              p != end; ++p)
+    ofstream myfile;
+    myfile.open ("test.txt");
+    myfile << std::fixed;
+    myfile << T;
+    myfile.close();
+    chrono.printTime();
 
-//    int temp2 = 0;
-//    for (Triangulation::Finite_facets_iterator it = T.finite_facets_begin(), end = T.finite_facets_end(); it!= end ; ++it)
-//    for (Triangulation::Finite_cells_iterator it = T.finite_cells_begin(), end = T.finite_cells_end(); it!= end ; ++it)
-//    {
-//        Triangulation::Facet tem = *it;
-//        Triangulation::Triangle triangle = T.triangle(tem);
-//        it->vertex(0);
-//        cout << it->vertex(0)->point() << " " << it->vertex(1)->point() << " " << it->vertex(2)->point() << endl;
-//        cout << triangle << endl;
-//        return 0;
-//        L.
-//        cout << it->vertex(0)->info()<<endl;
-//        cout << temp2 << endl;
-//        temp2++;
-//    }
-//    cout << T.number_of_edges() << endl;
+
     cout << "Triangulation delaunay end" << endl;
     cout << "Write ply file start" << endl;
 
-//    ofstream fout("data/output.ply",  ios::out);
-//    fout << "ply" <<endl;
-//    fout << "format binary_little_endian 1.0" << endl; // binary
-//    fout << "comment author: Etienne Frank University of applied science HES-SO" << endl;
-//    fout << "element vertex "<< nb_points << endl;
-//    fout << "property float x" << endl;
-//    fout << "property float y" << endl;
-//    fout << "property float z" << endl;
-//    fout << "element face "<< 0/*T.number_of_cells()/**/ << endl;
-//    fout << "property list uchar int32 vertex_index" << endl;
-//    fout << "end_header" << endl;
-//    fout.close();
-//    ofstream fout2("data/output.ply",  ios::out| ios::binary| ios::app);
-//    float temp;
-//    for(unsigned int i=0;i<nb_points;i++)
-//    {
-//        temp = convertIntFloat(L[i].x());
-//        fout2.write((char *) &temp, sizeof(temp));
-//        temp = convertIntFloat(L[i].y());
-//        fout2.write((char *) &temp, sizeof(temp));
-//        temp = convertIntFloat(L[i].z());
-//        fout2.write((char *) &temp, sizeof(temp));
-//    }
-//    fout2.close();
+    ofstream fout("data/output.ply",  ios::out);
+    fout << "ply" <<endl;
+    fout << "format binary_little_endian 1.0" << endl; // binary
+    fout << "comment author: Etienne Frank University of applied science HES-SO" << endl;
+    fout << "element vertex "<< T.number_of_vertices() << endl;
+    fout << "property float x" << endl;
+    fout << "property float y" << endl;
+    fout << "property float z" << endl;
+    fout << "element face "<< 0/*T.number_of_cells()/**/ << endl;
+    fout << "property list uchar int32 vertex_index" << endl;
+    fout << "end_header" << endl;
+    fout.close();
+    ofstream fout2("data/output.ply",  ios::out| ios::binary| ios::app);
+    ifstream finTemp("test.txt",ios::out);
+
+    string line;
+    getline(finTemp,line);
+    getline(finTemp,line);
+    float xTemp,yTemp,zTemp;
+    for(unsigned int i=0;i<T.number_of_vertices();i++)
+    {
+        finTemp >> xTemp >> yTemp >> zTemp;
+        fout2.write((char *) &xTemp, sizeof(xTemp));
+        fout2.write((char *) &yTemp, sizeof(yTemp));
+        fout2.write((char *) &zTemp, sizeof(zTemp));
+    }
+    getline(finTemp,line);
+    unsigned char listLength = 4;
+    int32_t idx1,idx2,idx3,idx4;
+    for(unsigned int i=0;i<T.number_of_cells();i++)
+    {
+        finTemp >> idx1 >> idx2 >> idx3 >> idx4;
+//        fout2.write((char *) &listLength, sizeof(listLength));
+//        fout2.write((char *) &idx1, sizeof(idx1));
+//        fout2.write((char *) &idx2, sizeof(idx2));
+//        fout2.write((char *) &idx3, sizeof(idx3));
+//        fout2.write((char *) &idx4, sizeof(idx4));
+        cout << i << " " << idx1 << " " << idx2 << " " << idx3 << " " << idx4 << endl;
+    }
+
+
+    cout << "number of cells" << T.number_of_cells()<<endl;
+    cout << "number of vertices" << T.number_of_vertices()<<endl;
+
+    fout2.close();
 
     cout << "PLY file writed" << endl;
 
